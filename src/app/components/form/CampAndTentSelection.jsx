42bcbb0cf20dd5150e4fc3e00471b5bf914a4455
Melbye <simo80k3@stud.kea.dk>
2024-05-28 11:29:03 +0200

    axios

--- src/app/components/form/CampAndTentSelection.jsx
+++ src/app/components/form/CampAndTentSelection.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import CampButton from '@/app/components/form/CampButton';
 import ShoppingCart from '@/app/components/form/ShoppingCart';
-import axios from 'axios';
+const axios =require('axios');
 import supabase from '@/app/lib/supabaseClient';
 
 const CampAndTentSelection = ({ formData, setFormData, nextStage, prevStage, handleCampSelection, handleInputChange, errors, setErrors, calculateTotalPrice }) => {
